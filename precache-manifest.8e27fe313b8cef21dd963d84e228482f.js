627aaf206cb932640051f684576d650469e10b7a
yairweiss1 <yair.we21@gmail.com>
2020-08-03 23:11:43 +0300

    Updates

---
+++ precache-manifest.8e27fe313b8cef21dd963d84e228482f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ede264f1f00bb25e3a24fb663a67af11",
+    "url": "/bitcoin-app-react/static/media/bitcoin-bg.ede264f1.jpg"
+  },
+  {
+    "revision": "d087e8311d5c78dce170de09761a879b",
+    "url": "/bitcoin-app-react/static/media/Exo-Regular.d087e831.ttf"
+  },
+  {
+    "revision": "25b232f2f84e4b4fcc702d5e8d9d4adb",
+    "url": "/bitcoin-app-react/static/media/Bitcoin.25b232f2.png"
+  },
+  {
+    "revision": "417f692296f1268a0101",
+    "url": "/bitcoin-app-react/static/js/runtime~main.417f6922.js"
+  },
+  {
+    "revision": "3924f4bc928efc0d8b1c",
+    "url": "/bitcoin-app-react/static/js/main.3924f4bc.chunk.js"
+  },
+  {
+    "revision": "099c3c9b9e3fc86a6a9d",
+    "url": "/bitcoin-app-react/static/js/1.099c3c9b.chunk.js"
+  },
+  {
+    "revision": "3924f4bc928efc0d8b1c",
+    "url": "/bitcoin-app-react/static/css/main.0c543b78.chunk.css"
+  },
+  {
+    "revision": "96f91c595d6a04937beb08ce5204a0ce",
+    "url": "/bitcoin-app-react/index.html"
+  }
+];
